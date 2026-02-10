bbe38ea2de0e2d5ae0ded3cb55d0617f91608b7d
delvis Sanabria <134345133+DelvisSanabria@users.noreply.github.com>
2026-02-10 09:56:07 -0400

    Update settings.py

--- firstry/firstry/settings.py
+++ firstry/firstry/settings.py
@@ -144,7 +144,7 @@ REST_FRAMEWORK = {
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
